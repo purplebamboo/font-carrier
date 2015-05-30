6d68d5333ce4eed4f3aca058737539a36df59916
weikang.zhuwk <weikang.zhuwk@alibaba-inc.com>
2015-05-31 02:24:20 +0800

    init

--- test/index.js
+++ test/index.js
@@ -1,8 +1,62 @@
-
+var fs = require('fs')
 //测试新建字体
 
-var fontCarrier = require('../lib/lib.js')
-var font = fontCarrier.create()
-font.setGlyph('我','<svg xmlns="http://www.w3.org/2000/svg" width="1000" height="1000" viewBox="10 10 1000 1000"><circle cx="500" cy="500" r="20"/><circle cx="500" cy="500" r="60"/></svg>')
+var fontCarrier = require('../lib/index.js')
 
-console.log(font.toString())
+var circle = fs.readFileSync('./test/svgs/circle.svg').toString()
+var love = fs.readFileSync('./test/svgs/love.svg').toString()
+var mail = fs.readFileSync('./test/svgs/mail.svg').toString()
+
+
+//创建空白字体，使用svg生成字体
+var font = fontCarrier.create()
+
+font.setGlyph('爱',{
+  svg:love,
+  glyphName:'爱'
+})
+
+font.setSvg('&#xe600;',circle)
+font.setSvg('&#xe601;',mail)
+//console.log(font.toString())
+
+font.output({
+  path:'./test/font1'
+})
+
+console.log('由于需要读取转换2m的方正字体，所以会很慢。。。')
+
+//测试从其他字体获取字形
+var transFont = fontCarrier.transfer('./test/test.ttf')
+var a = transFont.getGlyph('&#xFF0C;')
+
+font.setGlyph('哈',a)
+
+console.log('开始解析方正字体。。。')
+var transFont2 = fontCarrier.transfer('./test/fz.ttf')
+console.log('解析结束，实际使用中一般不会解析这么大的字体库。。。')
+
+//测试对象set
+var gs = transFont2.getGlyph('我是方正')
+font.setGlyph(gs)
+
+font.output({
+  path:'./test/font2'
+})
+
+
+
+//测试精简字体
+transFont2.min('我是精简后的字体，我可以重复')
+transFont2.output({
+  path:'./test/font3'
+})
+
+
+//导出字形
+//var g = font.getGlyph('我')
+//g.toSvg('./test/export.svg')
+
+var path = font.getSvg('我')
+fs.writeFileSync('./test/export.svg',path)
+
